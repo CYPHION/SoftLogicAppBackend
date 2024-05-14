bac07af87b91faa8f231f458b65f94ea1f200917
fawadCyphion <147619623+fawadCyphion@users.noreply.github.com>
2024-05-14 17:32:06 +0500

    briefForm multer done

--- controller/briefForm.controller.js
+++ controller/briefForm.controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("../utils/asyncHandler");
 const briefFormServices = require("../services/briefForm.service")
 exports.createBriefForm = asyncHandler(async (req, res, next) => {
+    console.log(req.body)
     const data = await briefFormServices.createForm(req.body)
 
     return res.send({
--- model/briefForm.model.js
+++ model/briefForm.model.js
@@ -6,17 +6,34 @@ const BriefForm = db.define("BriefForm", {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // validate: {
+        //     isEmail: {
+        //         args: true,
+        //         msg: 'Please enter a valid email address.',
+        //     }
+        // }
+    },
     formData: {
         type: DataTypes.TEXT,
         get: function () {
-            return JSON.parse(this.getDataValue('value'));
+            return JSON.parse(this.getDataValue('formData')); // Changed 'value' to 'formData'
         },
         set: function (value) {
-            this.setDataValue('value', JSON.stringify(value));
+            this.setDataValue('formData', JSON.stringify(value)); // Changed 'value' to 'formData'
         },
         allowNull: false,
     },
-    image: {
+    formImage: {
+        type: DataTypes.STRING
+    },
+    userImage: {
         type: DataTypes.STRING
     }
 });
