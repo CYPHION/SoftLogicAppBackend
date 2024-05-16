dbff749421a05b689914e541ad61585b1af30791
fawadCyphion <147619623+fawadCyphion@users.noreply.github.com>
2024-05-16 17:48:20 +0500

    bried-forms completed

--- controller/briefForm.controller.js
+++ controller/briefForm.controller.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("../utils/asyncHandler");
 const briefFormServices = require("../services/briefForm.service")
 exports.createBriefForm = asyncHandler(async (req, res, next) => {
-    console.log(req.body)
     const data = await briefFormServices.createForm(req.body)
 
     return res.send({
